9e7f20874eb2924ebc5548fbb15605f42a8450a2
kbeni <benedekkonya005@gmail.com>
2023-05-19 09:44:16 +0200

    first part

---
+++ Pilotak/Pilota.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilotak
+{
+    public class Pilota
+    {
+        private string név;
+        private string szültési_dátum;
+        private string nemzetiség;
+        private int rajtszám;
+        private int év;
+
+        public string Név { get => név; set => név = value; }
+        public string Szültési_dátum { get => szültési_dátum; set => szültési_dátum = value; }
+        public string Nemzetiség { get => nemzetiség; set => nemzetiség = value; }
+        public int Rajtszám { get => rajtszám; set => rajtszám = value; }
+        public int Év { get => év; set => év = value; }
+    }
+}
--- Pilotak/Program.cs
+++ Pilotak/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Pilotak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
